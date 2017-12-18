b971f6c0acb44269bfc94ad6b4a565c940d85ca8
SenatoR913 <34660768+SenatoR913@users.noreply.github.com>
2017-12-18 22:37:53 +0300

    Add files via upload

---
+++ SquareAPI/SquareAPI/SquareApi.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+
+
+
+namespace SquareAPI
+{
+    public class SquareApi
+    {
+       public int GetRightTriangleSquare(int b, int a, int c) { return a * b / 2; }
+    }
+   
+}
---
+++ UnitTestProject/UnitTestProject/UnitTest1.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using SquareAPI;
+
+namespace UnitTestProject
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void GetRightTriangleSquareTest1()
+        {
+            SquareApi sa = new SquareApi();
+            Assert.AreEqual(24, sa.GetRightTriangleSquare(10,8,6));
+            Assert.AreEqual(44, sa.GetRightTriangleSquare(9, 11, 16));
+        }
+
+    }
+}
